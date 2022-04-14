27e5e5695990b47ffbcf069445bcd10efc6e8d57
Jheovany Menjivar <jheovany.menjivarg@gmail.com>
2022-04-13 19:38:44 -0600

    first commit

---
+++ setup-humhub-server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "HubHub Server Setup"
+
+sudo apt update
+sudo apt install -y apache2
+sudo apt install -y php php-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring
+sudo systemctl restart apache2
+#sudo apt install -y mysql-server
+#sudo mysql_secure_installation
+
+FILE=humhub-1.10.3.tar.gz
+FOLDER=humhub-1.10.3
+
+if [ ! -f $FILE ]; then 
+	wget https://www.humhub.com/download/package/$FILE
+	tar xvfz $FILE
+fi
+
+if [ -f $FOLDER ]; then
+	sudo mv $FOLDER /var/www/humhub
+	sudo chown -R www-data:www-data /var/www/humhub
+fi
