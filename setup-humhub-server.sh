0cf66eb8da7356968f204b78c769b78178f39dda
Jheovany Menjivar <jheovany.menjivarg@gmail.com>
2022-04-14 09:30:21 -0600

    apache file conf

--- setup-humhub-server.sh
+++ setup-humhub-server.sh
@@ -5,7 +5,7 @@ echo "https://docs.humhub.org/docs/admin/installation"
 
 sudo apt update
 sudo apt install -y apache2
-sudo apt install -y php php-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring
+sudo apt install -y php php-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-zip php7.4-intl
 sudo systemctl restart apache2
 #sudo apt install -y mysql-server
 #sudo mysql_secure_installation
