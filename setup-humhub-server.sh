310a2736b3de08b450f931b67aa7b5b07b5ef7ef
Jheovany Menjivar <ejmenjiv@bancolombia.com.co>
2022-04-16 09:50:40 -0600

    added apache configuration

--- setup-humhub-server.sh
+++ setup-humhub-server.sh
@@ -5,9 +5,8 @@ echo "https://docs.humhub.org/docs/admin/installation"
 
 sudo apt update
 sudo apt install -y apache2
-sudo apt install -y php php-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-zip php7.4-intl
-sudo systemctl restart apache2
-#sudo apt install -y mysql-server
+sudo apt install -y php php-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring
+sudo apt install -y mysql-server
 #sudo mysql_secure_installation
 
 FILE=humhub-1.10.3.tar.gz
@@ -18,7 +17,17 @@ if [ ! -f $FILE ]; then
 	tar xvfz $FILE
 fi
 
-if [ -f $FOLDER ]; then
+if [ -d $FOLDER ]; then
 	sudo mv $FOLDER /var/www/humhub
 	sudo chown -R www-data:www-data /var/www/humhub
 fi
+
+# Configure Apache Server
+sudo ufw allow 'Apache'
+sudo cp humhub.conf /etc/apache2/sites-available/
+sudo a2ensite humhub.conf
+sudo a2dissite 000-default.conf
+sudo systemctl restart apache2
+
+# Display hostname
+hostname –I
